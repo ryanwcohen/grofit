04cd90d18e8f74b17ee3ef14e69ba8b12c120986
Ryan Cohen <ryan.wade.cohen@gmail.com>
2018-10-30 13:02:38 -0400

    Do an actual calculation and output for ayatan sculptures.

--- Gemfile
+++ Gemfile
@@ -1,5 +1,6 @@
 gem "httparty"
 gem "pry", "~> 0.11.3"
 gem "sinatra"
+gem "sinatra-contrib"
 
 source "https://rubygems.org"
--- app.rb
+++ app.rb
@@ -1,6 +1,7 @@
 require "httparty"
 require "pry"
 require "sinatra"
+require "sinatra/reloader" if development?
 
 set :bind, "0.0.0.0"
 
@@ -9,4 +10,29 @@ get "/" do
   @first = response["payload"]["statistics_closed"]["90days"][0]
   @second = response["payload"]["statistics_closed"]["90days"][1]
   erb :item
+end
+
+get "/endo" do
+  @results = {}
+  sculptures = {
+    "anasa" => 3450,
+    "orta" => 2700,
+    "vaya" => 1800,
+    "piv" => 1725,
+    "valana" => 1575,
+    "sah" => 1500,
+    "ayr" => 1425
+  }
+  sculptures.keys.each do |sculpture|
+    address ="https://api.warframe.market/v1/items/#{sculpture}_ayatan_sculpture/statistics"
+    response = HTTParty.get(address)
+    source = response["payload"]["statistics_closed"]["90days"][0]
+    @results[sculpture] = {
+      endo: sculptures[sculpture],
+      wa_price: source["wa_price"],
+      endo_plat: sculptures[sculpture] / source["wa_price"],
+      volume: source["volume"]
+    }
+  end
+  erb :ayatans
 end
